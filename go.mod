b1804565c94699eb1bc74bee542a9d78c31cd7c0
myJPQ <1051095242@qq.com>
2020-03-14 20:55:23 +0800

    2.2

---
+++ go.mod
@@ -0,0 +1,6 @@
+module github.com/myJPQ/gqlgen-todos
+
+require (
+	github.com/99designs/gqlgen v0.11.3
+	github.com/vektah/gqlparser/v2 v2.0.1
+)
---
+++ graph/resolver.go
@@ -0,0 +1,10 @@
+package graph
+//go:generate go run github.com/99designs/gqlgen
+import "github.com/myJPQ/gqlgen-todos/graph/model"
+// This file will not be regenerated automatically.
+//
+// It serves as dependency injection for your app, add any dependencies you require here.
+
+type Resolver struct{
+	todos []*model.Todo
+}
